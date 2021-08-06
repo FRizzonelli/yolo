ecfa02925ce3296e9f2d7a295ebd819023b577c0
annalisafilippi94 <annalisa.filippi1994@gmail.com>
2021-08-07 01:38:11 +0200

    alternate comp. fixed

--- src/containers/Landing/AlternateStepComp.tsx
+++ src/containers/Landing/AlternateStepComp.tsx
@@ -12,7 +12,7 @@ const AlternateStepComponent: FunctionComponent<IAlternateStep> = ({ side, image
     return (
       <div className='mt-24'>
         <div className='lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24'>
-          <div className='px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0'>
+          <div className='px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0 items-center flex'>
             <div>
               <div>
                 <span className='h-12 w-12 rounded-md flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600'>
@@ -20,7 +20,7 @@ const AlternateStepComponent: FunctionComponent<IAlternateStep> = ({ side, image
                 </span>
               </div>
               <div className='mt-6'>
-                <h2 className='text-3xl font-extrabold tracking-tight text-gray-900'>Stay on top of customer support</h2>
+                <h2 className='text-3xl font-extrabold tracking-tight text-gray-900'>{description}</h2>
                 <p className='mt-4 text-lg text-gray-500'>
                   Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia porttitor. Pulvinar laoreet sagittis viverra
                   duis. In venenatis sem arcu pretium pharetra at. Lectus viverra dui tellus ornare pharetra.
@@ -29,12 +29,8 @@ const AlternateStepComponent: FunctionComponent<IAlternateStep> = ({ side, image
             </div>
           </div>
           <div className='mt-12 sm:mt-16 lg:mt-0'>
-            <div className='pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full'>
-              <img
-                className='w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none'
-                src={image}
-                alt='Inbox user interface'
-              />
+            <div className='pl-4 -mr-48 sm:pl-6 md:-mr-16 '>
+              <img className='w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5' src={image} />
             </div>
           </div>
         </div>
@@ -44,7 +40,7 @@ const AlternateStepComponent: FunctionComponent<IAlternateStep> = ({ side, image
     return (
       <div className='mt-24'>
         <div className='lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24'>
-          <div className='px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2'>
+          <div className='px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2 items-center flex'>
             <div>
               <div>
                 <span className='h-12 w-12 rounded-md flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600'>
@@ -52,18 +48,14 @@ const AlternateStepComponent: FunctionComponent<IAlternateStep> = ({ side, image
                 </span>
               </div>
               <div className='mt-6'>
-                <h2 className='text-3xl font-extrabold tracking-tight text-gray-900'>Better understand your customers</h2>
-                <p className='mt-4 text-lg text-gray-500'>{description}</p>
+                <h2 className='text-3xl font-extrabold tracking-tight text-gray-900'>{description}</h2>
+                <p className='mt-4 text-lg text-gray-500'></p>
               </div>
             </div>
           </div>
           <div className='mt-12 sm:mt-16 lg:mt-0'>
-            <div className='pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full'>
-              <img
-                className='w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none'
-                src={image}
-                alt='Customer profile user interface'
-              />
+            <div className='pr-4 -ml-48 sm:pr-6 md:-ml-16'>
+              <img className='w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5' src={image} />
             </div>
           </div>
         </div>
